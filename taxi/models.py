3f2c15b7bd514685e79946ef7b66302d8920b439
SPMSPI <kislicin.artur@gmail.com>
2026-05-30 08:55:04 +0300

    ready

---
+++ taxi/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from taxi_service import settings
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=128, unique=True)
+
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    country = models.CharField(max_length=128)
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=128)
+    manufacturer = models.ForeignKey(
+        Manufacturer, on_delete=models.CASCADE, related_name="Manufacturer"
+    )
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="Drivers")
